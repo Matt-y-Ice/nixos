60f168a0c4bb71ec6647f2ce80dd218966456b66
matty ice <matty_ice_2011@pm.me>
2025-03-20 16:40:44 -0400

    enabled os prober for dual boot capability

--- hosts/gnome/configuration.nix
+++ hosts/gnome/configuration.nix
@@ -10,6 +10,10 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot.configurationLimit = 5;
+  boot.loader.grub.useOSProber = true;
+  boot.loader.grub.efiSupport = true;
+  boot.loader.grub.device = "nodev";
 
   networking.hostName = "nix-desktop"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
