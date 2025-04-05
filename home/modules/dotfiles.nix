f494b0c3bb698651c863219123206c2f1f9e7232
matty ice <matty_ice_2011@pm.me>
2025-04-04 22:11:41 -0400

    changed paths in dotfiles.nix

--- home/modules/dotfiles.nix
+++ home/modules/dotfiles.nix
@@ -1,19 +1,11 @@
 { inputs, ... }:
 
-let
-  dotfiles = "${inputs.self}/home/dotfiles";
-in
 {
-  # This module is used to manage dotfiles in the home directory.
-  # It uses the `home.file` and `xdg.configFile` options to create symlinks
-  # to the dotfiles in the specified source directory.
-
-  home.file = {
-    ".vimrc".source = "${dotfiles}/vimrc";
+  home.file.".vimrc".source = builtins.path {
+    path = "${inputs.self}/home/dotfiles/vimrc";
   };
 
-  xdg.configFile = {
-    "containers/policy.json".source = "${dotfiles}/policy.json";
+  xdg.configFile."containers/policy.json".source = builtins.path {
+    path = "${inputs.self}/home/dotfiles/policy.json";
   };
 }
-
