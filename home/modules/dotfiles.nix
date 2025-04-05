d175c2d7d0b5745e034d4ddc6123b443009b2ed6
matty ice <matty_ice_2011@pm.me>
2025-04-04 22:19:14 -0400

    changed dotfiles.nix dotfile paths

--- home/modules/dotfiles.nix
+++ home/modules/dotfiles.nix
@@ -7,5 +7,5 @@ in {
     builtins.readFile (builtins.path { path = "${dotfiles}/vimrc"; });
 
   xdg.configFile."containers/policy.json".text =
-    builtins.readFile (builtins.path { path = "${dotfiles}/containers/policy.json"; });
+    builtins.readFile (builtins.path { path = "${dotfiles}/policy.json"; });
 }
