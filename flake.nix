2d98d24b103c5a0145245fb002e85806e8bf6575
matty ice <matty_ice_2011@pm.me>
2025-04-04 22:53:19 -0400

    flake: auto-select host configuration based on system hostname

--- flake.nix
+++ flake.nix
@@ -10,16 +10,17 @@
   };
 
   outputs = { self, nixpkgs, home-manager, ... }@inputs:
-    let 
+    let
       system = "x86_64-linux";
-      hostname = builtins.readFile "/etc/hostname";
+      hostname = builtins.readFile ./hostname;
       cleanedHostname = builtins.replaceStrings ["\n"] [""] hostname;
+      pkgs = import nixpkgs { inherit system; };
     in {
       nixosConfigurations.${cleanedHostname} = nixpkgs.lib.nixosSystem {
         inherit system;
         specialArgs = { inherit inputs; };
         modules = [
-          ./hosts/${hostname}/configuration.nix
+          ./hosts/${cleanedHostname}/configuration.nix
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
