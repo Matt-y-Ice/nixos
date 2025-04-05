88c5f6d00cd760bf7b12c7a9e9fad50d57c93d31
matty ice <matty_ice_2011@pm.me>
2025-04-04 21:27:02 -0400

    changed home-manager import path in flake.nix

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
         {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
-          home-manager.users.mattyice = import ./home.nix;
+          home-manager.users.mattyice = import ./home/home.nix;
         }
       ];
     };
