f73b731fd178746a156b82d768963e7c939af5b5
matty ice <matty_ice_2011@pm.me>
2025-04-06 16:49:56 -0400

    nix-desktop: broke out system packages into packages.nix form config

--- hosts/nix-desktop/configuration.nix
+++ hosts/nix-desktop/configuration.nix
@@ -10,46 +10,6 @@
     ./modules/gnome.nix
     ./modules/audio.nix
     ./modules/users.nix
+    ./modules/packages.nix
   ];
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  # Enable the package manager and list system-level packages.
-  environment.systemPackages = with pkgs; [
-    vim
-    wireguard-tools
-    curl
-    wget
-    tree
-    flatpak
-    ptyxis
-    podman
-    podman-desktop
-    podman-compose
-    docker
-    docker-compose
-    # Virtualization packages
-    qemu
-    libvirt
-    virt-manager
-    swtpm
-    OVMF
-    dnsmasq
-    bridge-utils
-    usbutils
-    spice
-    cudatoolkit
-  ];
-
-  # Enable Flatpak
-  services.flatpak.enable = true;
-
-  # Enable Lorri
-  services.lorri.enable = true;
-
-  system.stateVersion = "24.11"; # Did you read the comment?
-
-  virtualisation.docker.enable = true;
-  systemd.services.docker = { enable = true; };
-
 }
---
+++ hosts/nix-desktop/modules/packages.nix
@@ -0,0 +1,25 @@
+{ pkgs, ... };
+
+{
+  nixpkgs.config.allowUnfree = true;
+  environment.systemPackages = with pkgs; [
+    # terminal tools
+    vim curl wget tree ptyxis
+    wireguard-tools
+
+    # container software
+    podman podman-desktop podman-compose
+    docker docker-compose
+
+    # virtualization software
+    qemu libvirt virt-manager swtpm
+    OVMF dnsmasq bridge-utils usbutils
+    spice
+
+    # cuda
+    cudatoolkit
+  ];
+
+  services.flatpak.enable = true;
+  services.lorri.enable = true;
+}
