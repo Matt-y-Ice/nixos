2744e14e04ceed308a6f8778436c0dcbdaf53bd3
matty ice <matty_ice_2011@pm.me>
2025-04-06 16:23:36 -0400

    nix-desktop: broke out general settings into general.nix form config

--- hosts/nix-desktop/configuration.nix
+++ hosts/nix-desktop/configuration.nix
@@ -5,27 +5,8 @@
     ./hardware-configuration.nix
     ./modules/boot.nix
     ./modules/networking.nix
+    ./modules/general.nix
   ];
-
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-  # Set your time zone.
-  time.timeZone = "America/New_York";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
-  };
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
@@ -47,9 +28,6 @@
     layout = "us";
     variant = "";
   };
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
 
   # Enable sound with pipewire.
   services.pulseaudio.enable = false;
