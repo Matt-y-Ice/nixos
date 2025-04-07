7e65624b27ee5febe6c402d7666427c70229e67e
matty ice <matty_ice_2011@pm.me>
2025-04-07 15:13:00 -0400

    auto init fish and starship

--- flake.nix
+++ flake.nix
@@ -24,6 +24,7 @@
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.extraSpecialArgs = { inherit inputs; };
+            home-manager.backupFileExtension = "backup";
             home-manager.users.mattyice = {
               imports = [ ./home/home.nix ];
             };
